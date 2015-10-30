ed80786fa990b4ad28e68de6d28fdb2191513bd1
misoton665 <misoton998@hotmail.co.jp>
2015-10-31 06:10:59 +0900

    Update HelpCommand.scala to show description of all commands.

--- src/main/scala/com/misoton/misoline/command/HelpCommand.scala
+++ src/main/scala/com/misoton/misoline/command/HelpCommand.scala
@@ -3,6 +3,15 @@ package com.misoton.misoline.command
 case object HelpCommand extends Command{
   override def run(args: List[String]): CommandResult = {
     println("Hi! I am a help command!")
+    println("create [id] [name] [phone] [email]: Create new user as having parameters.\n" +
+            "checkout [id]                     : Checkout User have [id]\n" +
+            "exit                              : Exit from the MisoLine.\n" +
+            "follow (phone|email) [param]      : Follow user by phone number or email address.\n" +
+            "friends                           : Show your(was check outed) friends.\n" +
+            "help                              : ME!\n" +
+            "message [id] [body]               : Send message [body] to [to].\n" +
+            "show [id]                         : Show [id]'s talk messages.\n" +
+            "users                             : Show all users.\n")
     CommandResult(CommandResult.OK, "Call Help.")
   }
 }
